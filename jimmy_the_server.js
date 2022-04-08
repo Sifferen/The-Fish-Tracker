e9ec62123b7b0395dab68bc9769ab087c5adb0db
jack Thompson <jackt8685@gmail.com>
2022-04-07 20:00:05 -0700

    comit add css

--- jimmy_the_server.js
+++ jimmy_the_server.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = new express();
+const bodyParser = require("body-parser");
+
 const path = require('path')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.listen(3000,function () {
-        console.log('started');
+        console.log('mehhhhhh');
     })
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, 'public/jimmythe.html'));
+        res.sendFile(path.join(__dirname, 'public/jimmythelanding.html'));
       });
       
     
-    
+      app.get('/jimmytheregistration.html', function(req, res) {
+        res.sendFile(path.join(__dirname, 'public/jimmytheregistration.html'));
+      });
       
       
+      app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'public/jimmythelanding.html'));
+      });
+
+      app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'public/jimmythelanding.html'));
+      });
+
+      app.post('/jimmytheregistration.html', function(req, res) {
+        console.log(req.body)
+        res.sendFile(path.join(__dirname, 'public/jimmytheregistration.html'));
+      });
---
+++ public/jimmytheaccountant.js
@@ -0,0 +1,3 @@
+
+function fsh(){
+    console.log("Hit <a href='jimmythelogin.html'>THIS BUTTON</a> and return to the login page and login with your new password.")
