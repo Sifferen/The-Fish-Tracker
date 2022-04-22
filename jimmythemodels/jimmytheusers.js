00ad26648552dc3b4488b11ffb181d72a37fbcc7
jack Thompson <jackt8685@gmail.com>
2022-04-21 19:29:00 -0700

    remove node_modules

---
+++ jimmythemodels/jimmytheusers.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose')
+const schema= mongoose.Schema
+const userSchema = new Schema({
+    username: { type: String, required: true},
+    email: { type: String, required: true},
+    password: { type: String, required: true}
+}, {collection: 'users'})
+
+const user= mongoose.model('Users',userSchema)
+
+module.exports=user
+
+
+
+
+
