d9a19522d471295e82668da9fc6d62baecc9cbbd
Logan-Dang <logandang100@yahoo.com>
2022-09-14 23:22:49 -0700

    bcrypt

--- jimmytheserver.js
+++ jimmytheserver.js
@@ -6,8 +6,12 @@ const Usermodel = require("./jimmythemodels/jimmytheusers");
 const User = Usermodel.User;
 const userRouter = require("./jimmytheroutes/jimmythepath");
 const path = require("path");
+const { genSalt, hash } = require("bcrypt");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/", userRouter);
+app.set("view engine", "ejs");
+
 const url = "mongodb://localhost:27017/jimmy";
 mongoose.connect(
   url,
@@ -17,9 +21,6 @@ mongoose.connect(
     console.log("mehhhhhhhhhhhhhhhhhhhhhhhhhhh");
   }
 );
-app.use("/", userRouter);
-app.set("view engine", "ejs");
-
 app.listen(3000, function () {
   console.log("mehhhhhh");
 });
@@ -40,9 +41,18 @@ app.get("/jimmythelogin.html", function (req, res) {
 });
 
 app.post("/jimmytheregistration.html", function (req, res) {
-  const newUser = new User(req.body);
-  newUser.save();
-  res.sendFile(path.join(__dirname, "public/jimmytheregistration.html"));
+  const saltRounds = 10;
+  const { password } = req.body;
+  genSalt(saltRounds, (err, salt) => {
+    hash(password, salt, (err, hash) => {
+      const newUser = new User({
+        ...req.body,
+        password: hash
+      });
+      newUser.save();
+      res.redirect(302, "/jimmythe")
+    })
+  })
 });
 app.post("/jimmythe.html", function (req, res) {
   const { username, password } = req.body;
